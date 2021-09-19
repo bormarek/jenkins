bd8e0321ef54d64855c7a6ff80c157448a400166
Marek <bormarek@gmail.com>
2021-09-19 15:29:28 +0200

    backup commit

---
+++ startJenkinsAgent.sh
@@ -0,0 +1,1 @@
+java -jar agent.jar -jnlpUrl http://localhost:8080/computer/agent1/jenkins-agent.jnlp -secret 089a2f5f02dd7e4e849dd98f7cef5e956c27bc45d5d5e863d7d9be6687295155 -workDir "/home/marek/"
